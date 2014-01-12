990c89a733fd86e0adced448e91d25f5a2d1ae26
Martin Gross <martin@pc-coholic.de>
2014-01-12 13:12:19 +0100

    Updated ticket-updater-script

--- unused_nodes/mvgdefas/ticker/getmvgticker.py
+++ unused_nodes/mvgdefas/ticker/getmvgticker.py
@@ -4,10 +4,12 @@ import io
 
 feed = feedparser.parse("http://www.mvg-mobil.de/Tickerrss/CreateRssClass")
 
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
+
 jsonfeed = []
 
 for item in feed['items']:
   jsonfeed.append(item['title'])
 
-with io.open('mvgticker.json', 'w', encoding='utf-8') as f:
+with io.open(BASEDIR + '/mvgticker.json', 'w', encoding='utf-8') as f:
   f.write(unicode(json.dumps(jsonfeed, ensure_ascii=False)))
