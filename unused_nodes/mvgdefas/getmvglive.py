e9f1d1003fd17f2a6b0989169c9d4a13e9e93f29
Martin Gross <martin@pc-coholic.de>
2014-01-12 13:10:29 +0100

    Directory-independant working version of departure-updater

--- unused_nodes/mvgdefas/getmvglive.py
+++ unused_nodes/mvgdefas/getmvglive.py
@@ -1,32 +1,33 @@
 #-*- coding: UTF-8 -*-
-import commands
 import io
+import os
 import json
+import MVGLive
 
-stations = {}
-stations['Sandstraße'] = {"default_transports": ["tram"]}
-stations['Theresienstraße'] = {"default_transports": ["u"]}
-stations['Görresstraße'] = {"default_transports": ["bus"]}
+stations = []
+stations.append('Sandstraße')
+stations.append('Theresienstraße')
+stations.append('Görresstraße')
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
 
 alldepartures = []
 departures = []
 for station in stations:
-  with io.open('.mvg', 'w', encoding='utf-8') as f:
-    f.write(unicode(json.dumps(stations[station], ensure_ascii=False)))
-  alldepartures.append(json.loads(commands.getoutput('mvg_json ' + station)))
+  alldepartures.append(MVGLive.getlivedata(station))
 
-commands.getoutput('rm .mvg')
 
 collected = 0
 currentdeparture = 0
 while collected < 9:
   for departureset in alldepartures:
-    for departure in departureset['result_display']:
-      if departure['minutes'] == currentdeparture:
+    for departure in departureset:
+      if departure['time'] == currentdeparture:
         departures.append(departure)
         collected += 1
   currentdeparture += 1
 
-with io.open('departures.json', 'w', encoding='utf-8') as f:
+
+with io.open(BASEDIR + '/departures.json', 'w', encoding='utf-8') as f:
    f.write(unicode(json.dumps(departures, ensure_ascii=False)))
 
