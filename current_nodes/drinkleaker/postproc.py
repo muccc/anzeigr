5e10cc7e7acba33316c447ab1caf596053fa4d0d
Franz Pletz <fpletz@fnordicwalking.de>
2014-01-18 05:48:15 +0100

    drinkleaker: remove drink filter

--- current_nodes/drinkleaker/postproc.py
+++ current_nodes/drinkleaker/postproc.py
@@ -3,16 +3,11 @@
 import sys
 import json
 
-DRINKS = (u'Mate', u'Shots', u'Bier', u'Tschunk')
-
 rows = json.load(sys.stdin)['rows']
 drinks = map(lambda x: (x['key'][1], x['value']), rows)
 
 drinks_sums = {}
 for (drink_name, value) in drinks:
-    if drink_name not in DRINKS:
-        continue
-
     if drink_name in drinks_sums:
         drinks_sums[drink_name] += value
     else:
