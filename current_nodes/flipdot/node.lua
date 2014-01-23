52591db95c4af3c64a5febb6d20d54746000be46
Andy Mueller <andz@gmx.de>
2014-01-23 19:26:53 +0100

    added pic

---
+++ current_nodes/flipdot/node.lua
@@ -0,0 +1,15 @@
+gl.setup(1360, 768)
+
+image = resource.load_image("kannste.png")
+
+
+function node.render()
+    gl.clear(0, 0, 0, 1) -- black background
+
+    -- render logo
+    --resource.render_child("header"):draw(0, 0, WIDTH, 100)
+
+    -- show current flipdot img
+    util.draw_correct(image, 0, 0, WIDTH, HEIGHT)
+
+end
