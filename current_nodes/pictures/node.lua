9e3cf94d30b949285caf4ccd93cc680a1cc2ca3e
Andy Mueller <andz@gmx.de>
2014-06-01 17:38:20 +0200

    picture test

--- current_nodes/pictures/node.lua
+++ current_nodes/pictures/node.lua
@@ -1,7 +1,23 @@
+local INTERVAL = 10
+
 gl.setup(1024, 768)
 
-image = resource.load_image("kannste.png")
+pictures = util.generator(function()
+    local out = {}
+    for name, _ in pairs(CONTENTS) do
+        if name:match(".*jpg") then
+            out[#out + 1] = name
+        end
+    end
+    return out
+end)
+node.event("content_remove", pictures.remove)
 
+util.set_interval(INTERVAL, function()
+    local next_image_name = pictures.next()
+    print("now showing " .. next_image_name)
+    current_image = resource.load_image(next_image_name)
+end)
 
 function node.render()
     gl.clear(0, 0, 0, 1) -- black background
@@ -9,7 +25,8 @@ function node.render()
     -- render logo
     resource.render_child("header"):draw(0, 0, WIDTH, 100)
 
-    -- show current flipdot img
-    util.draw_correct(image, 0, 100, WIDTH, HEIGHT)
+    -- show images
+    util.draw_correct(current_image, 0, 0, WIDTH, HEIGHT)
 
 end
+
