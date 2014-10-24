d7b754a7ce570ad09c5375ed69a5bf4ac6bc5cf3
anzeigr <anzeiger@muc.ccc>
2014-10-24 16:59:07 +0000

    ffmuc anzeige auf andere datenquelle umgebaut

--- current_nodes/ffmuc_state/node.lua
+++ current_nodes/ffmuc_state/node.lua
@@ -4,12 +4,9 @@ json = require "json"
 
 font = resource.load_font("silkscreen.ttf")
 
-util.loaders.json = function(filename)
-    return json.decode(resource.load_file(filename))
-end
+str_stats = resource.load_file("node_stats")
 
 util.auto_loader(_G)
-
 
 function node.render()
     gl.clear(1, 1, 1, 1) -- white background
@@ -17,27 +14,14 @@ function node.render()
     -- render logo
     resource.render_child("header"):draw(0, 0, WIDTH, 100)
 
-    font:write(75, 250, "Nodes Online", 80, 0,0,0,1)
-    font:write(75, 450, "Gateways Online", 80, 0,0,0,1)
-    font:write(75, 650, "Clients Connected", 80, 0,0,0,1)
+    font:write(75, 300, "Nodes Online", 80, 0,0,0,1)
+    font:write(75, 600, "Clients Connected", 80, 0,0,0,1)
 
-    nodecount=0
-    gatewaycount=0
-    clients=0
-    for Index,Value in pairs(nodes.nodes) do
-	if(Value.flags.online) == true then	
-	  if(Value.flags.client) == true then
-            clients = clients+1
-          elseif(Value.flags.gateway) == true then
-            gatewaycount = gatewaycount + 1
-          else
-            nodecount = nodecount + 1          
-          end
-        end
-    end
+    router = 0
+    clients = 0
+    router, clients = string.match(str_stats, '(%d+) Router, (%d+)')
 
-    font:write(75, 550, clients, 100, 0,0,0,1)
-    font:write(75, 350, gatewaycount, 100, 0,0,0,1)    
-    font:write(75, 150, nodecount, 100, 0,0,0,1)
+    font:write(75, 150, router, 160, 0,0,0,1)    
+    font:write(75, 450, clients, 160, 0,0,0,1)
 
 end
