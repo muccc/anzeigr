ea2dd9414c40bc05d008913efe2b15d356597487
Stefan `Sec` Zehl <sec@42.org>
2017-05-09 22:02:52 +0200

    Add linebreak in long events

--- current_nodes/kalenderscraper/node.lua
+++ current_nodes/kalenderscraper/node.lua
@@ -17,10 +17,30 @@ function node.render()
     -- render logo
     resource.render_child("header"):draw(0, 0, WIDTH, 100)
 
-    -- show next event
-    font:write(50, 150, nextevent.weekday, 100, 1,1,1,1)
-    font:write(50, 300, nextevent.date, 100, 1,1,1,1)
-    font:write(50, 450, nextevent.time, 100, 1,1,1,1)
-    font:write(50, 600, nextevent.name, 100, 1,1,0,1)
+    -- show next event with linebreak
+    size=100
+    offset=50
+    font:write(offset, 150, nextevent.weekday, size, 1,1,1,1)
+    font:write(offset, 300, nextevent.date, size, 1,1,1,1)
+    font:write(offset, 450, nextevent.time, size, 1,1,1,1)
 
+    if (font:width(nextevent.name,size)+offset>WIDTH) then
+      o=nextevent.name:len()
+      while (o) and font:width(nextevent.name:sub(1,o),size)>WIDTH do
+        o=nextevent.name:sub(1,o-1):match(".*%s()")
+        if o then
+          o=o-1
+        end
+      end
+      if not o then
+        o=nextevent.name:len()
+      end
+      while o>0 and font:width(nextevent.name:sub(1,o),size)>WIDTH do
+        o=o-1
+      end
+      font:write(50, 600, nextevent.name:sub(1,o), 100, 1,1,0,1)
+      font:write(50, 680, nextevent.name:sub(o+1), 100, 1,1,0,1)
+    else
+      font:write(50, 600, nextevent.name, 100, 1,1,0,1)
+    end
 end
